3c916265a75a218515ddd366352e6f910970d499
cfbz <135277408+cfbz@users.noreply.github.com>
2025-07-07 08:53:16 -0400

    recomendacionai fotos etiqueta.ts

---
+++ src/etiqueta.ts
@@ -0,0 +1,27 @@
+
+export interface EtiquetaForAi {
+  id: number;
+  nombre: string;
+  imagen: string; 
+}
+
+
+export const EtiquetaForAi: EtiquetaForAi[] = [
+  { id: 1, nombre: "Arte", imagen: "/assets/actividades/pintura.jpg" },
+  { id: 2, nombre: "Deporte", imagen: "/assets/actividades/correr.jpg" },
+  { id: 3, nombre: "Música", imagen: "/assets/actividades/baile_airelibre.jpg" },
+  { id: 4, nombre: "Familia", imagen: "/assets/actividades/lastarria.jpg" },
+  { id: 5, nombre: "Educación", imagen: "/assets/actividades/lectura_airelibre.jpg" },
+  { id: 6, nombre: "Social", imagen: "/assets/actividades/lastarria.jpg" },
+  { id: 7, nombre: "Aventura", imagen: "/assets/actividades/senderismo.jpg" },
+  { id: 8, nombre: "Relajación", imagen: "/assets/actividades/meditacion.jpg" },
+  { id: 9, nombre: "Naturaleza", imagen: "/assets/actividades/parque_forestal.jpg" },
+  { id: 10, nombre: "Cultura", imagen: "/assets/actividades/tour_fotografico.jpg" },
+  { id: 11, nombre: "Salud", imagen: "/assets/actividades/entrenamiento.jpg" },
+  { id: 12, nombre: "Turismo", imagen: "/assets/actividades/fotografia.jpg" },
+  { id: 13, nombre: "Bienestar", imagen: "/assets/actividades/meditacion.jpg" },
+  { id: 14, nombre: "Ecología", imagen: "/assets/actividades/ramuntcho.jpg" },
+  { id: 15, nombre: "Vacaciones", imagen: "/assets/actividades/campamento_nocturno.jpg" },
+  { id: 16, nombre: "Convivencia", imagen: "/assets/actividades/convivencia.jpg" },
+  { id: 17, nombre: "Relaciones", imagen: "/assets/actividades/picnic_parque.jpg" },
+];
